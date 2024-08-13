046e02500ad36a1ad6dddc54613857b93d244865
Keigo <sliverkeigo343@gmail.com>
2024-08-13 14:29:11 +0800

    Update Gmerss.py

--- Gmerss.py
+++ Gmerss.py
@@ -10,59 +10,11 @@ displayMax=2 # 每个RSS最多抓取数
 weeklyKeyWord="" # 周刊过滤关键字
 
 rssBase={
-    "独立开发变现":{
-        "url":"https://www.ezindie.com/feed/rss.xml",
-        "type":"weekly",
-        "timeFormat":"%a, %d %b %Y %H:%M:%S GMT",
-        "nameColor":"#a4244b"
-    },
-    "夜枫":{
-        "url":"https://yefengs.com/feed",
-        "type":"post",
-        "timeFormat":"%a, %d %b %Y %H:%M:%S +0000",
-        "nameColor":"#b8d101"
-    },
-    "kn007":{
-        "url":"https://kn007.net/feed/",
-        "type":"post",
-        "timeFormat":"%a, %d %b %Y %H:%M:%S +0000",
-        "nameColor":"#e76976"
-    },
-    "二丫讲梵":{
-        "url":"https://wiki.eryajf.net/learning-weekly.xml",
-        "type":"weekly",
-        "timeFormat":"%a, %d %b %Y %H:%M:%S +0800",
-        "nameColor":"#93bd76"
-    },
-    "豌豆花下猫":{
-        "url":"https://pythoncat.top/rss.xml",
-        "type":"weekly",
-        "timeFormat":"%a, %d %b %Y %H:%M:%S GMT",
-        "nameColor":"#bc4c00"
-    },
-    "1Link":{
-        "url":"https://1link.fun/index.xml",
-        "type":"weekly",
-        "timeFormat":"%a, %d %b %Y %H:%M:%S +0000",
-        "nameColor":"#7479dc"
-    },
     "阮一峰":{
         "url":"http://www.ruanyifeng.com/blog/atom.xml",
         "type":"weekly",
         "timeFormat":"%Y-%m-%dT%H:%M:%SZ",
         "nameColor":"#1f883d"
-    },
-    "老胡的周刊":{
-        "url":"https://weekly.howie6879.com/rss/rss.xml",
-        "type":"weekly",
-        "timeFormat":"%a, %d %b %Y %H:%M:%S +0806",
-        "nameColor":"#A333D0"
-    },
-    "Meekdai":{
-        "url":"https://blog.meekdai.com/rss.xml",
-        "type":"post",
-        "timeFormat":"%a, %d %b %Y %H:%M:%S +0000",
-        "nameColor":"#df7150"
     }
 }
 ######################################################################################
